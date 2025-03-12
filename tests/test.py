97d4206fe4c03d7d0e70c8ba06070db167379d3a
Hrithik S <hrithik.siko@gmail.com>
2025-03-11 22:04:44 -0400

    Update test.py
    Add feature to select between 2 search modes (speech and text)

--- tests/test.py
+++ tests/test.py
@@ -6,17 +6,36 @@ os.environ["OMP_NUM_THREADS"] = "1"
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import torch
-from src.dataset_loader import load_images, encode_text, search_similar, image_filenames, index
+from src.dataset_loader import load_images, encode_text, search_similar, transcribe_audio, image_filenames, index
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 load_images()
 
-query_text = "Serene nature" # sample text for now
+print("\nSelect input mode for search")
+print("1. Text")
+print("2. Voice")
+
+while True:
+    try:
+        input_mode = int(input("\nEnter mode: ").strip())
+
+        if input_mode == 1:
+            query_text = input("\nEnter your text query: ").strip()
+            break
+        elif input_mode == 2:
+            query_text = transcribe_audio()
+            # print(f"\nTranscribed Query: {query_text}")
+            break
+        else:
+            print("Invalid input! Please enter 1 or 2.")
+    except ValueError:
+        print("Invalid input! Please enter a number (1 or 2).")
+
 
 query_embedding = encode_text([query_text])
 
-print("Query Embedding (first 5 values):", query_embedding[0][:5]) 
+# print("Query Embedding (first 5 values):", query_embedding[0][:5]) 
 
 indices = search_similar(query_embedding, top_k=5)
 
